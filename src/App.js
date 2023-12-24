1179840cfca6ce8fdca4b2740621c1c85154ded6
Yury Chaikouski <klaidik92@mail.ru>
2023-12-24 20:02:28 +0100

    Настроить проект

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<div>123456</div>
+		</Div>
+	);
+};
